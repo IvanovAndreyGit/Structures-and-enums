5bfb9849e28a5c5f0c9e05f93cb486b5a15491d5
IvanovAndreyGit <ivanov.andrey.croc@gmail.com>
2023-05-22 23:28:04 +0300

    Добавьте файлы проекта.

---
+++ Task1/Task1.cpp
@@ -0,0 +1,90 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+enum months {
+    Jan = 1,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec
+            };
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+
+  
+    do
+    {
+        std::cout << "Введите номер месяца: ";
+        std::cin >> n;
+        if (0 <= n && n < 13)
+        {
+            switch (n)
+            {
+            case (months::Jan):
+                std::cout << "Январь\n";
+                break;
+            case (months::Feb):
+                std::cout << "Февраль\n";
+                break;
+            case (months::Mar):
+                std::cout << "Март\n";
+                break;
+            case (months::Apr):
+                std::cout << "Апрель\n";
+                break;
+            case (months::May):
+                std::cout << "Май\n";
+                break;
+            case (months::Jun):
+                std::cout << "Июнь\n";
+                break;
+            case (months::Jul):
+                std::cout << "Июль\n";
+                break;
+            case (months::Aug):
+                std::cout << "Август\n";
+                break;
+            case (months::Sep):
+                std::cout << "Сентябрь\n";
+                break;
+            case (months::Oct):
+                std::cout << "Октябрь\n";
+                break;
+            case (months::Nov):
+                std::cout << "Ноябрь\n";
+                break;
+            case (months::Dec):
+                std::cout << "Декабрь\n";
+                break;
+            case (0):
+                std::cout << "До свидания";
+                exit;
+            }
+        }
+        else
+        {
+            std::cout << "Неправильный номер!" << std::endl;
+        }
+    } while (n);
+    
+    //else
+    //{ 
+    //    std::cout << "До свидания" << std::endl;
+    //}
+ 
+    return 0;
+
+}
+
